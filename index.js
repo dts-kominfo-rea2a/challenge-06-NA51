0ceda32d4bca87d63af835c13ac19dce3fed3c8c
NA51 <nasi8713@gmail.com>
2022-06-12 19:59:44 +0700

    fixxing error index js

--- index.js
+++ index.js
@@ -1,53 +1,16 @@
-// TODO: import module bila dibutuhkan di sini
-const fs = require("fs");
+const { bacaData } = require("./fungsiCustom");
 
 // ! JANGAN DIMODIFIKASI
-let file1 = "./data1.json";
-let file2 = "./data2.json";
-let file3 = "./data3.json";
-
-// ! JANGAN DIMODIFIKASI
-let modifyFile1 = (val) => {
-  file1 = val;
-};
-let modifyFile2 = (val) => {
-  file2 = val;
-};
-let modifyFile3 = (val) => {
-  file3 = val;
-};
-
-// TODO: Kerjakan bacaData
-// gunakan variabel file1, file2, dan file3
-// let files = [file1, file2, file3];
-const bacaData = (fnCallback) => {
-  const result = [];
-
-  fs.readFile(file1, { encoding: "utf8" }, (err, data) => {
-    const content = JSON.parse(data);
-    message = content.message;
-    result.push(message.split(" ")[1]);
-
-    fs.readFile(file2, { encoding: "utf8" }, (err, data) => {
-      const content = JSON.parse(data);
-      message = content[0].message;
-      result.push(message.split(" ")[1]);
-
-      fs.readFile(file3, { encoding: "utf8" }, (err, data) => {
-        const content = JSON.parse(data);
-        message = content[0].data.message;
-        result.push(message.split(" ")[1]);
-
-        console.log(result);
-        fnCallback(err, result);
-      });
-    });
+// main function
+(() => {
+  console.log("Mari kita membaca data yah !");
+  console.log("====");
+  bacaData((err, data) => {
+    if (err) return console.log(err);
+    console.log(data?.join());
   });
-};
-// ! JANGAN DIMODIFIKASI
-module.exports = {
-  modifyFile1,
-  modifyFile2,
-  modifyFile3,
-  bacaData,
-};
+  console.log("Walaupun tulisan ini diletakkan di setelah bacaData");
+  console.log("Namun tulisan ini akan ditampilkan terlebih dahulu");
+  console.log("Karena bacaData bersifat asynchronous");
+  console.log("====");
+})();
